8f43187e6493674257437e56e1802da626954584
aylaesilva <ayla.e.silva@ba.estudante.senai.br>
2026-02-26 20:31:13 -0300

    aprendendo if e switch

---
+++ 5.if_else_if.js
@@ -0,0 +1,11 @@
+// Condicionais.
+
+const idade = 60
+
+if (idade > 18) {
+    console.log("Maioridade.")
+} else if (idade >= 14){
+    console.log("Adolescente.")
+} else {
+    console.log("Infância. ")
+}
