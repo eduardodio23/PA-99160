a9737a1204d92ffd211eaedd55d7ad3c42d888d9
aylaesilva <ayla.e.silva@ba.estudante.senai.br>
2026-02-26 21:39:05 -0300

    aprendendo funcao

---
+++ 10.funcao.js
@@ -0,0 +1,15 @@
+// FUNCAO
+
+function somar(a,b){
+    return a + b
+}
+
+function subtrair(a,b){
+    return a - b
+}
+
+const soma = somar(2,3)
+console.log(`Soma: ${soma}`)
+
+consts subtrair = somar(2,3)
+const soma = somar(2,3)
