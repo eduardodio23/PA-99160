e389c6eea275302865cfb4d91e9a4f35a4124bf5
eduardo <eduardo.f.jesus@ba.estudante.senai.br>
2026-02-26 22:16:26 -0300

    fazendo função

--- 10.funcao.js
+++ 10.funcao.js
@@ -8,8 +8,26 @@ function subtrair(a,b){
     return a - b
 }
 
+function multi(a,b){
+    return a * b
+}
+
+function dividir(a,b){
+    if(b === 0){
+        return 'Não é possível dividir por zero'
+    }
+    return a / b
+}
+
+
 const soma = somar(2,3)
 console.log(`Soma: ${soma}`)
 
-consts subtrair = somar(2,3)
-const soma = somar(2,3)
+const subtracao = subtrair(5,3)
+console.log(`Subtração: ${subtracao}`)
+
+const multiplicacao = multi(2,3)
+console.log(`Multiplicação: ${multiplicacao}`)
+
+const divisao = dividir(10,2)
+console.log(`Divisão: ${divisao}`)
