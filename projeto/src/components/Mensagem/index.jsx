10830a3e82721b490b6987ac2b11a34c897d9216
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-26 21:19:10 -0300

    aprendendo react

---
+++ projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,10 @@
+import './style.css'
+
+export default function Mensagem() {
+    return (
+        <div className='Mensagem'>
+            <h1>Componente</h1>
+            <p>Este é um Componente simples.</p>
+        </div>
+    )
+}
